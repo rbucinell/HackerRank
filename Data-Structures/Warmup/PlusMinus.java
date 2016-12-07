d2445590449102063320be122f8eff43e4d67c91
Ryan Bucinell <rbucinell.dev@gmail.com>
2016-12-07 17:26:26 -0500

    Fixed spacing

--- Data-Structures/Warmup/PlusMinus.java
+++ Data-Structures/Warmup/PlusMinus.java
@@ -9,12 +9,16 @@ import java.util.regex.*;
 **/
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+	{
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-        double pos = 0, neg = 0 , zeros = 0;
-        for(int arr_i=0; arr_i < n; arr_i++){
+        
+		double pos = 0, neg = 0 , zeros = 0;
+        
+		for(int arr_i=0; arr_i < n; arr_i++)
+		{
             arr[arr_i] = in.nextInt();
             if ( arr[arr_i] == 0)
                 zeros++;
