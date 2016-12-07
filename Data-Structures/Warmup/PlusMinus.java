cf33b998eb12a2560bfaeef71819a0f4ae5059b8
Ryan Bucinell <rbucinel@paychex.com>
2016-12-07 16:50:04 -0500

    Solved Plus Minus

---
+++ Data-Structures/Warmup/PlusMinus.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        double pos = 0, neg = 0 , zeros = 0;
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+            if ( arr[arr_i] == 0)
+                zeros++;
+            else if( arr[arr_i] < 0 )
+                neg++;
+            else
+                pos++;
+        }
+        
+        System.out.println(  pos/(double)n);
+        System.out.println(  neg/(double)n);
+        System.out.println( zeros/(double)n);
+        
+    }
+}
