0251cd8467eae33380ae0c9fef0ff6cfe9d1a401
Ryan Bucinell <rbucinell.dev@gmail.com>
2016-12-08 14:39:04 -0500

    solved Reverse Print linked list

---
+++ Data-Structures/Linked-Lists/ReversePrint.java
@@ -0,0 +1,37 @@
+public class ReversePrint
+{
+	//https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse
+	
+	/*
+	Insert Node at the end of a linked list 
+	head pointer input could be NULL as well for empty list
+	Node is defined as 
+	class Node {
+		int data;
+		Node next;
+	}
+	*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+
+	void ReversePrint(Node head) 
+	{
+		// This is a "method-only" submission. 
+		// You only need to complete this method. 
+		Stack<Integer> stack = new Stack<Integer>();
+		Node pointer = head;
+		while( pointer != null )
+		{
+			stack.push( pointer.data );
+			pointer.next = pointer;
+			
+		}
+
+		for( int i = 0; i < stack.size();i++ )
+		{
+			System.out.println( stack.pop() );
+		}
+
+	}
+
+}
