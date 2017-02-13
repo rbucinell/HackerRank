411dc103d904cf2aca0cbb374a21829d82396213
Ryan Bucinell <rbucinell.dev@gmail.com>
2017-02-13 12:58:56 -0500

    with M, a-b and k, having large numbers brute force doesn't work. using slopes now

--- Data-Structures/Arrays/Crush.java
+++ Data-Structures/Arrays/Crush.java
@@ -8,31 +8,38 @@ public class Crush {
 
     public static void main(String[] args) {
         
-		Scanner sc = new Scanner( System.in );
+		/*
+			The trick is not to add every k to get an exact value between a and b. What you do is add k to index a and subtract k from b+1. This creates a slope for the changes at the edges of every (a-b)m. 
+			Then starting from 0 add the slopes from the indexes. Wherever the sum was the highest is you're max value since all values started at 0 to begin with. 
+			This brings the time complexity of the algorithm from quadratic O( n^2 )  to constant O( n ). Actually O(2n) since we have to walk through it to find the max.
+		*/
+		
+        Scanner sc = new Scanner( System.in );
         long N = sc.nextLong();
         long M = sc.nextLong();
-                
-        HashMap<Long, Long> map = new HashMap<Long, Long>();        
-		long max = 0;
 		
-        for( long i = 0; i < M; i++ )
+        HashMap<Long, Long> map = new HashMap<Long, Long>();        
+		long max = 0, sum = 0;
+		
+		for( long i = 0; i < M; i++ )
         {
-            long a = sc.nextLong(), b = sc.nextLong(), k = sc.nextLong();
+			long a = sc.nextLong(), b = sc.nextLong(), k = sc.nextLong();
+			Long aVal = map.get( a-1 );
+			map.put( a-1, (aVal != null ) ? aVal + k : k );
 			
-			if( a == b )
+			Long bVal = map.get( b );
+			map.put( b, (bVal != null ) ? bVal - k : -k );
+		}
+		
+		for( long i = 0; i < N; i++ )
+		{
+			Long val = map.get( i );
+			if( val != null )
 			{
-				map.put( a - 1, map.containsKey( a-1) ? map.get( a-1 ) + k : k );
-				if( map.get(a-1) > max ) max = map.get(a - 1 );
+				sum += val;
+				if( sum > max ) max = sum;
 			}
-			else
-			{		
-				for( long j = a - 1; j <= b - 1; j++ )
-				{
-					map.put( j, map.containsKey( j ) ? map.get( j ) + k : k );
-					if( map.get(j) > max ) max = map.get(j);
-				}  
-			}
-        }        
-        System.out.println( max );
+		}
+		System.out.println( max );
     }
 }
