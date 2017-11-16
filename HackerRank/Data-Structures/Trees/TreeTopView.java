9d197c386aae7e6269522ed19dd66afd8dcde0d9
Bucinell, Ryan <rbucinell.dev@gmail.com>
2017-11-16 11:03:16 -0500

    fixed subprocess to alter print statements depending on left/right path

--- HackerRank/Data-Structures/Trees/TreeTopView.java
+++ HackerRank/Data-Structures/Trees/TreeTopView.java
@@ -2,5 +2,30 @@
 
 public class TreeTopView
 {
-    
+    void topView( Node root )
+    {
+        if( root != null )
+        {
+            topView( root.left, false );
+            System.out.print( root.data + " " );
+            topView( root.right, true );
+        }
+    }
+
+    void topView( Node root, boolean dir )
+    {
+        if( root != null )
+        {
+            if( dir )
+            {
+                System.out.print( root.data + " " );
+                topView( root.right, dir );
+            }
+            else
+            {
+                topView(root.left, dir );
+                System.out.print( root.data + " " );
+            }            
+        }
+    }
 }
