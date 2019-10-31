a2a2850af957db19849b3ebcbd3b99a563ccd30a
Ryan Bucinell <rbucinell.dev@gmail.com>
2019-10-31 14:54:23 -0400

    solved prob18

--- ProjectEuler/Problem18/sumpath.py
+++ ProjectEuler/Problem18/sumpath.py
@@ -1,17 +1,35 @@
-import math
-
 def readInNumbers( filepath ):
-    numbers = []
+    pyramid = []
     with open( filepath ) as fp:
         line = fp.readline()
         while line:
-            row = line.strip().split( ' ')
-            for i in row:   
-                numbers.append( {int(i), None} )
+            row = line.strip().split(' ')
+            numbers = []
+            for i in row:
+                numbers.append({ 'val': int(i), 'sum': None })
+            pyramid.append( numbers )
             line = fp.readline()
-    return numbers
-    
+    return pyramid
 
-numbers = readInNumbers('pyramid.txt')
-numbers[0][1] = numbers[0][0]
-print( numbers[0] )
+def left( r, c ):
+    return r + 1, c
+def right( r, c ):
+    return r+1, c+1
+
+def sum( pyr, row, col ):
+    if row >= len(pyr) or col >= len(pyr[row]):
+        return 0
+    if pyr[row][col]['sum'] is not None:
+        return pyr[row][col]['sum']
+    else:
+        maxChild = max( sum( pyr, *left( row, col )), sum( pyr, *right( row, col )) )
+        pyr[row][col]['sum'] = pyr[row][col]['val'] + maxChild
+        return pyr[row][col]['sum']
+
+def solveFile( path ):
+    nums = readInNumbers( path )
+    print( f'Answer: {sum( nums, 0, 0 )}')
+
+
+#solveFile( 'example.txt' )
+solveFile( 'pyramid.txt' )
